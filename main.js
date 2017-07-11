6f6fc6fb52aae12ee9070eaece4f4f67a90fcee7
MASHA <mashanikit@gmail.com>
2017-07-11 17:52:09 -0400

    currently on question 8 and it doesn't work

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/*
+  Hey, welcome to the first question!
+
+  Here's a breakdown of the question, and some pointers on how to get started!
+    - A variable has been declared a few lines above (`numSales`).
+    - Just below, the contents of the `numSales` variable are logged to the console.
+    - Your job is to assign the variable to the correct value (in this case: the total number of sales) *BEFORE* it is logged out.
+    - You can do this by:
+      - Adding an `=` sign (we are *assigning* something after all)
+      - Starting with the `transactions` variable (see the example question);
+      - Adding one or more methods to transform/extract the value we're looking for.
+      - If your solution is correct, `numSales` should be equal to 5.
+
+  You can solve the remaining questions in the same way!
+
+  P.S.
+  The breakdown above takes up a lot of space, feel free to move it to the top or bottom of the file!
+*/
 var transactions = [
   {
     type: 'sale',
@@ -129,26 +147,13 @@ console.log( 'The total number of transactions is:', totalTransactions );
   HINT(S):
   - Not all transactions are 'sales'.
 */
-var numSales;
+var numSales = 0;
 
-/*
-  Hey, welcome to the first question!
-
-  Here's a breakdown of the question, and some pointers on how to get started!
-    - A variable has been declared a few lines above (`numSales`).
-    - Just below, the contents of the `numSales` variable are logged to the console.
-    - Your job is to assign the variable to the correct value (in this case: the total number of sales) *BEFORE* it is logged out.
-    - You can do this by:
-      - Adding an `=` sign (we are *assigning* something after all)
-      - Starting with the `transactions` variable (see the example question);
-      - Adding one or more methods to transform/extract the value we're looking for.
-      - If your solution is correct, `numSales` should be equal to 5.
-
-  You can solve the remaining questions in the same way!
-
-  P.S.
-  The breakdown above takes up a lot of space, feel free to move it to the top or bottom of the file!
-*/
+var sales = transactions.filter(function (transaction) {
+  if(transaction.type === 'sale') {
+    numSales++;
+  }
+});
 
 console.log( 'The total number of sales is:', numSales );
 
@@ -159,7 +164,15 @@ console.log( 'The total number of sales is:', numSales );
 /*
   Calculate the total number of 'purchases'.
 */
-var numPurchases;
+var numPurchases = 0;
+
+var purchases = transactions.filter(isPurchase)
+
+function isPurchase(transaction) {
+  if(transaction.type === 'purchase') {
+    numPurchases++;
+  }
+}
 
 console.log( 'The total number of purchases is:', numPurchases );
 
@@ -173,8 +186,15 @@ console.log( 'The total number of purchases is:', numPurchases );
   HINT(S):
   - Don't forget that 'purchases' can also be made in 'cash'!
 */
-var numCashSales;
+var numCashSales = 0;
 
+var cashSales = transactions.filter(isCashSales)
+
+function isCashSales(transaction) {
+  if (transaction.type === 'sale' && transaction.paymentMethod === 'cash'){
+    numCashSales++;
+  }
+}
 console.log( 'The total number of cash sales is:', numCashSales );
 
 
@@ -187,7 +207,15 @@ console.log( 'The total number of cash sales is:', numCashSales );
   HINT(S):
   - Make sure to exclude any 'sales' made by 'credit'!
 */
-var numCreditPurchases;
+var numCreditPurchases = 0;
+
+var creditPurchases = transactions.filter(isCreditPurchase)
+
+function isCreditPurchase(transaction) {
+  if (transaction.type === 'purchase' && transaction.paymentMethod === 'credit'){
+    numCreditPurchases++;
+  }
+}
 
 console.log( 'The total number of credit purchases is:', numCreditPurchases );
 
@@ -204,10 +232,20 @@ console.log( 'The total number of credit purchases is:', numCreditPurchases );
   - The assembled array should be made up of strings, not full `transaction` objects.
   - This array is allowed to contain duplicate values.
 */
-var uniqueVendors;
 
-console.log( 'The unique vendors are:', uniqueVendors );
+var uniqueVendors = [];
 
+function hasVendors(transaction) {
+  if (transaction.vendor) {
+    uniqueVendors.push(transaction.vendor)
+  }
+};
+
+transactions.forEach(hasVendors);
+
+
+
+console.log( 'The unique vendors are: ', uniqueVendors );
 
 // --------------------------------------------------
 // QUESTION 06
@@ -221,7 +259,16 @@ console.log( 'The unique vendors are:', uniqueVendors );
   - The assembled array should be made up of strings, not full `transaction` objects.
   - Make sure that the resulting array *does not* include any duplicates.
 */
-var uniqueCustomers;
+var uniqueCustomers = [];
+
+function hasCustomers(transaction) {
+  if (transaction.customer && !uniqueCustomers.includes(transaction.customer)) {
+    uniqueCustomers.push(transaction.customer)
+  }
+};
+
+transactions.forEach(hasCustomers);
+
 
 console.log( 'The unique customers are:', uniqueCustomers );
 
@@ -239,7 +286,18 @@ console.log( 'The unique customers are:', uniqueCustomers );
   - There may be more than 1 'sale' that includes 5 or more items.
   - Individual transactions do not have either `name` or `numItems` properties, we'll have to add them to the output.
 */
-var bigSpenders;
+var bigSpenders = [];
+
+transactions.filter(isBigSpender);
+
+function isBigSpender(transaction) {
+  if (transaction.items.length >= 5) {
+    numItems = transaction.items.length + ' items';
+    customerName = transaction.customer
+    bigSpenders.push(customerName, numItems);
+  }
+}
+
 
 console.log( 'The "big spenders" are:', bigSpenders );
 
